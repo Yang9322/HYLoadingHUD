953fc7a230bcd5cbbac1523bde5dd4f512d278b5
Yang9322 <576421791@qq.com>
2016-01-04 17:52:40 +0800

    Initial Commit

---
+++ HYLoadingHUD/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HYLoadingHUD
+//
+//  Created by He yang on 16/1/4.
+//  Copyright © 2016年 He yang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
